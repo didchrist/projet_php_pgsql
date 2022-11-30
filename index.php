9444abdb1680d33a4fe98a6f6522fa9fe1c0bbec
didchrist <christopher.pelette@gmail.com>
2022-11-30 16:06:42 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GROS ELEPHANT</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <header>
+        <div class="header-top">
+            <a href="#" class="logo">
+                <div class="logo-box">
+                    <p>Projet php</p>
+                </div>
+            </a>
+            <div>
+                <h1>Menu</h1>
+            </div>
+            <div></div>
+        </div>
+        <div class="header-bot">
+            
+            <ul>
+                <a href="#"><li>Accueil</li></a>
+                <a href="#"><li>Article</li></a>
+                <a href="#"><li>Client</li></a>
+                <a href="#"><li>Commande</li></a>
+                <a href="#"><li>Quitter</li></a>
+            </ul>
+        </div>
+    </header>
+    <section>
+        <p>block content</p>
+    </section>
+    <footer>
+        <p>© 2022 All right Reserved. Projet PHP</p>
+    </footer>
+</body>
+</html>
