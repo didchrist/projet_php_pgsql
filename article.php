7a29a04e7335ae69530e1b9c1d41766473764d52
didchrist <christopher.pelette@gmail.com>
2022-11-30 16:25:03 +0100

    ajout article

---
+++ article.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <section>
+        <h2>LISTE ARTICLE</h2>
+        <div>
+            <a href="#">Nouveau article</a>
+            <a href="#">Imprimer</a>
+        </div>
+        <div>
+            <p>CODE</p>
+            <p>DESIGNATION</p>
+            <p>PRIX Unitaire</p>
+            <p>Prix Revient</p>
+            <p>Actions</p>
+        </div>
+        <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+    </section>
+</body>
+</html>
